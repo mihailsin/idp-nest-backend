eebd4b027760f519f50b57945f7a0ddc946b2873
mihailsin <84630821+mihailsin@users.noreply.github.com>
2025-03-23 00:58:59 +0200

    port backup value added (#3)
    * added initial docker setup
    
    * added deploy.yml
    
    * fixed typo in deploy.yml
    
    * updated ci file
    
    * ci file edited
    
    * fixed typo
    
    * added backup port value

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-    await app.listen(process.env.PORT);
+    console.log({ PORT: process.env.PORT });
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
